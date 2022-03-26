c25f88486ccf90df5329487c50e0a35ccf902617
Ian293382 <moshou5571228@gmail.com>
2022-03-26 13:58:32 +0800

    implement group#update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,7 +19,19 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
-  
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find[paramers[:id]]
+
+    @group.update(group_params)
+
+    redirect_to groups_path, notice: "Update Success"
+  end
+
+
 
 
   private
