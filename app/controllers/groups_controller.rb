f0505f83c4cf48de7118148f4f549a662298bfac
Ian293382 <moshou5571228@gmail.com>
2022-03-27 15:46:06 +0800

    join and quit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :creat, :edit, :update ,:destroy]
+  before_action :authenticate_user! , only: [:new, :creat, :edit, :update ,:destroy, :join, :quit]
   before_action :find_group_and_check_permission, only: [:edit, :update,  :destroy]
   def index
     @groups = Group.all
@@ -48,6 +48,33 @@ class GroupsController < ApplicationController
     @group.destroy
     flash[:alert] = "Group delete"
     redirect_to groups_path
+  end
+
+
+  def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+        flash[:notice] = "Join Success!"
+      else
+        flash[:warning] = "U r already in Group"
+      end
+
+      redirect_to group_path(@group)
+    end
+
+
+    def quit
+      @group = Group.find(params[:id])
+
+      if current_user.is_member_of?(@group)
+        current_user.quit!(@group)
+        flash[:alert] = "U r already quit group"
+      else
+        flash[:warning] = " you r not member , how to quit "
+      end
+    redirect_to group_path(@group)
   end
 
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,10 +23,9 @@ class PostsController < ApplicationController
 
 
   private
-
   def post_params
-    params.require(:post).permit(:content)
-  end
+      params.require(:post).permit(:content)
+    end
 
 
 end
