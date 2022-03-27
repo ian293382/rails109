f9d7107ae798eabfc156fa7de40b2b144b4f24e9
Ian293382 <moshou5571228@gmail.com>
2022-03-27 23:52:49 +0800

    implement Post edit/update/destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
          has_many :participated_groups, :through => :group_relationships, :source => :group
 
          def is_member_of?(group)
-            participated_groups.include?(@group)
+            participated_groups.include?(group)
         end
 
         def join!(group)
